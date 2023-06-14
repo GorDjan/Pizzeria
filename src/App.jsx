a594192572839be5457768511ca2583dace4941a
GOR DURYAN <gorduryan7@gmail.com>
2023-06-14 23:26:05 +0400

    Router done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Routes, Route } from 'react-router-dom'
+
 import Header from './components/Header';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
-
+import { Cart } from './pages/Cart';
 import './scss/app.scss';
 
 //import pizzas from './assets/img/pizzas.json'
@@ -20,6 +21,9 @@ function App() {
 			<Routes>
 			
 			<Route path="/" element={<Home/>}/>
+			<Route path="/cart" element={<Cart/>}/>
+			<Route path="*" element={<NotFound/>}/>
+			
 			
 			</Routes>
           
