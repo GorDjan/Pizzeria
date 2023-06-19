80517579ea70ca24e3ad8deac8a9b22ef89cd5d9
GOR DURYAN <gorduryan7@gmail.com>
2023-06-19 22:22:32 +0400

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,7 @@ function Header(){
 					<div className="header__logo">
 						<img width="38" src={logoSvg} alt="Pizza logo" />
 						<div>
-						<h1>React Pizza</h1>
-						<p>самая вкусная пицца во вселенной</p>
+						<h1>Pizzeria</h1>
 						</div>
 					</div>
 			 </Link>
