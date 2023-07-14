383c316d7987b46f75faa73e0b15932f84c6eb60
GOR DURYAN <gorduryan7@gmail.com>
2023-07-14 16:48:02 +0400

    fix minus in cartSlice

--- src/redux/slices/cartSlice.jsx
+++ src/redux/slices/cartSlice.jsx
@@ -41,7 +41,7 @@ const cartSlice = createSlice({
 		},
 		minusItem(state,action){
 			const findItem = state.items.find((obj)=> obj.id === action.payload);
-			if(findItem && findItem.count >0){
+			if(findItem && findItem.count >1){
 				findItem.count-- 
 			}
 			state.totalPrice = state.items.reduce((sum,obj)=>{
